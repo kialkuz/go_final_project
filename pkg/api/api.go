c587aa1bfc02f23a4b5ac5b7b0a1fd2243fd7d68
Igor Kuzmin <kialkuz@yandex.ru>
2026-01-11 17:50:08 +0300

    added task handler

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -4,11 +4,9 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/Yandex-Practicum/final/pkg/dto"
 	"github.com/Yandex-Practicum/final/pkg/infrastructure/env"
-	"github.com/Yandex-Practicum/final/pkg/infrastructure/repository"
 )
 
 const (
@@ -22,30 +20,7 @@ func Init() *http.ServeMux {
 	mux.HandleFunc("GET /api/nextdate", nextDayHandler)
 	mux.HandleFunc("POST /api/task", addTaskHandler)
 	mux.HandleFunc("GET /api/tasks", tasksHandler)
-	mux.HandleFunc("GET /api/task", func(w http.ResponseWriter, r *http.Request) {
-		id := r.URL.Query().Get("id")
-		if id == "" {
-			log.Println("Не передан ID " + id)
-			writeJson(w, dto.ErrorResponse{ErrorText: "Не передан ID"})
-			return
-		}
-
-		numericId, err := strconv.Atoi(id)
-		if err != nil {
-			log.Println("Передан некорректный ID " + id)
-			writeJson(w, dto.ErrorResponse{ErrorText: "Передан некорректный ID"})
-			return
-		}
-
-		task, err := repository.GetTask(numericId)
-		if err != nil {
-			log.Println(err.Error())
-			writeJson(w, dto.ErrorResponse{ErrorText: "Задача не найдена"})
-			return
-		}
-
-		writeJson(w, task)
-	})
+	mux.HandleFunc("GET /api/task", taskHandler)
 	mux.HandleFunc("PUT /api/task", editTaskHandler)
 	mux.HandleFunc("DELETE /api/task", deleteTaskHandler)
 	mux.HandleFunc("POST /api/task/done", doneTaskHandler)
